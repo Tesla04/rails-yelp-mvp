97709987248170b5cf897362a470a4300e22a4e1
Niels Rupf <nrupf@uchicago.edu>
2020-08-12 14:18:09 +0200

    completed ex2 rake

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: {
+    in: (0..5).to_a,
+    message: 'This is not a valid rating'
+  }
+end
